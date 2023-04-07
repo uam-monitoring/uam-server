c3a568fafdbd3c2638cfc666482727fbce493f57
이한솔 <hansol8701@naver.com>
2023-04-07 16:23:22 +0900

    프로젝트 생성

---
+++ src/main/java/um/uamserver/UamserverApplication.java
@@ -0,0 +1,13 @@
+package um.uamserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UamserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UamserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/um/uamserver/UamserverApplicationTests.java
@@ -0,0 +1,13 @@
+package um.uamserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UamserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

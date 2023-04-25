a011ee240f44fa6ef1d5d6bf0c405a6b488458cc
이한솔 <hansol8701@naver.com>
2023-04-25 11:12:03 +0900

    feat: 병렬 처리

--- src/main/java/um/uamserver/domain/UamService.java
+++ src/main/java/um/uamserver/domain/UamService.java
@@ -3,6 +3,7 @@ package um.uamserver.domain;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import um.uamserver.domain.entity.RealTimePoint;
 import um.uamserver.domain.entity.Uam;
@@ -19,30 +20,13 @@ import static java.lang.Thread.sleep;
 @RequiredArgsConstructor
 public class UamService {
     private final UamRepository uamRepository;
-    private final ExecutorService executorService;
     private final RealTimePointProducer producer;
+
+    @Async
     public void depart(Long uamId) {
         Uam uam = uamRepository.findById(uamId).orElseThrow(CResourceNotFoundException::new);
         List<RealTimePoint> route = uam.getRoute();
-        log.info("start " +  uamId);
-        executorService.submit(() -> {
-            send(route);
-            log.info("{}번 UAM 주행 완료", uamId);
-        });
-    }
-
-    private void send(List<RealTimePoint> route) {
-        for (RealTimePoint point : route) {
-            producer.sendMessage(new DataFormat(point));
-            trySleep(TimeUnit.MILLISECONDS.convert(1, TimeUnit.SECONDS));
-        }
-    }
-
-    private void trySleep(long milli) {
-        try {
-            sleep(milli);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        log.info("start - {}", uamId);
+        producer.send(route);
     }
 }
---
+++ src/main/java/um/uamserver/global/config/AsyncConfig.java
@@ -0,0 +1,25 @@
+package um.uamserver.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("-kafka-async-service-");
+        executor.initialize();
+        return executor;
+    }
+}

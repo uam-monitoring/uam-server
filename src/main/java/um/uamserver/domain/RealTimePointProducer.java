5da21a8ddb059dd7cfeeee9bfaed8b7730dccd05
이한솔 <hansol8701@naver.com>
2023-05-09 00:22:27 +0900

    refactor: Fixm 데이터 형식 설정

--- src/main/java/um/uamserver/domain/RealTimePointProducer.java
+++ src/main/java/um/uamserver/domain/RealTimePointProducer.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import um.uamserver.domain.dto.DataFormat;
+import um.uamserver.domain.dto.AdsbData;
 import um.uamserver.domain.entity.uam.RealTimePoint;
 
 import java.util.List;
@@ -23,13 +23,13 @@ public class RealTimePointProducer {
     @Async
     void send(List<RealTimePoint> route) {
         for (RealTimePoint point : route) {
-            sendMessage(new DataFormat(point));
             trySleep(TimeUnit.MILLISECONDS.convert(1, TimeUnit.SECONDS));
+            sendMessage(new AdsbData(point));
         }
         log.info("end");
     }
 
-    private void sendMessage(DataFormat data) {
+    private void sendMessage(AdsbData data) {
         log.info("Produce message : {}", data);
         this.kafkaTemplate.send(TOPIC, data);
     }
